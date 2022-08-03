78007b29aac6be07ee0148acdbbd039e4df32021
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-08-03 20:52:35 -0300

    ADD: Criação dos modelos Board, Task e Column

--- natukanban/kanban/models.py
+++ natukanban/kanban/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# função para definir a data de vencimento da Task por padrão
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(days=7)
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=50)
+
+class Column(models.Model):
+    title = models.CharField(max_length=255)
+    board = models.ForeignKey("Board", related_name="columns", on_delete=models.CASCADE)
+
+class Task(models.Model):
+    
+    # (armazenado no banco de dados, visualização do usuário)
+    category_options = (
+        ('urgente', 'Urgente'),
+        ('importante', 'Importante'),
+        ('precisa ser feito', 'Precisa ser feito'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=one_week_hence)
+    category = models.CharField(max_length=25, choices=category_options, default='importante')
+    status = models.ForeignKey(Column, related_name="tasks", on_delete=models.CASCADE)
+
--- natukanban/kanban/views.py
+++ natukanban/kanban/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Board, Column, Task
 
-# Create your views here.
+def board_main(request):
+    main_board = Board.objects.all()
+    columns = Column.objects.all()
+    tasks = Task.objects.all()
+    # tarefas_pendentes = Tarefa.objects.filter(status='pendente')
+    return render(request, 'board/main_board.html', {'main_board':main_board, 'columns': columns, 'tasks':tasks})
+
