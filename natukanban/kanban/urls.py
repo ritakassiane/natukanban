9ad4af0b2ebebccf06a8cc5d979db7b1aa424d69
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-08-03 23:50:12 -0300

    ADD: Adc opção de deletar nas tasks

--- natukanban/kanban/urls.py
+++ natukanban/kanban/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.board_main, name='board_main' ),
     path('<int:task_id>/concluir/', views.done_task, name='done_task'),
+    path('<int:task_id>/excluir/', views.delete_task, name='delete_task'),
 ]
