ad47629ab0da4fd89d0e43ea7e36d60364120e47
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-08-03 23:03:31 -0300

    {ADD: Criação da view board main}

--- natukanban/kanban/views.py
+++ natukanban/kanban/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render, redirect
 from .models import Board, Column, Task
 
+def title_to_dict(title_list):
+    result = {}
+    for title in title_list:
+        result.setdefault(title, [])
+    return result
+
 def board_main(request):
     main_board = Board.objects.all()
     columns = Column.objects.all()
+    column_list = []
+
+    for column in columns:
+        column_list.append(column.title)
+    print(title_to_dict(column_list))
+
+    # tasks_ = {'In progress' : Task.objects.filter(status__title="In progress"),
+    # 'To do':Task.objects.filter(status__title="To do"),
+    # 'Do today':Task.objects.filter(status__title="Do Today"),
+    # 'Done':Task.objects.filter(status__title="Done")}
+    
+    # print(tasks_)
+
+    task_in_progress = Task.objects.filter(status__title="In progress")
+    task_to_do = Task.objects.filter(status__title="To do")
+    task_do_today = Task.objects.filter(status__title="Do Today")
+    task_done = Task.objects.filter(status__title="Done")
     tasks = Task.objects.all()
     # tarefas_pendentes = Tarefa.objects.filter(status='pendente')
-    return render(request, 'board/main_board.html', {'main_board':main_board, 'columns': columns, 'tasks':tasks})
+    return render(request, 'board/main_board.html', {'main_board':main_board, 'columns': columns, 'tasks':tasks, 'task_in_progress':task_in_progress, 'task_to_do': task_to_do, 'task_done': task_done, 'task_do_today': task_do_today})
 
